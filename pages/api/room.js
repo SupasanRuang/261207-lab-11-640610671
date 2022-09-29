de42cbdf76535b027216f57bd7f1003902fe795e
SupasanRuang <supasan_ruang@cmu.ac.th>
2022-09-29 15:31:00 +0700

    update

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,14 +2,23 @@ import { checkToken } from "../../backendLibs/checkToken";
 import { readChatRoomsDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
-  const user = checkToken(req);
-  if (!user) {
-    return res.status(401).json({
-      ok: false,
-      message: "Yon don't permission to access this api",
-    });
+  if (req.method === "GET") {
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
+    const chatrooms = readChatRoomsDB();
+    const result = [];
+    for (const room of chatrooms) {
+      result.push({
+        roomId: room.roomId,
+        roomName: room.roomName,
+      });
+    }
+    return res.json({ ok: true, rooms: result });
   }
-  const chatrooms = readChatRoomsDB();
-
   //create room data and return response
 }
--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -8,31 +8,65 @@ import { checkToken } from "../../../../backendLibs/checkToken";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     //check token
-
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
     //get roomId from url
     const roomId = req.query.roomId;
 
     const rooms = readChatRoomsDB();
-
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
     //check if roomId exist
-
+    if (roomIdx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      const listMessages = rooms[roomIdx].messages;
+      return res.json({ ok: true, messages: listMessages });
+    }
     //find room and return
     //...
   } else if (req.method === "POST") {
     //check token
-
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
     //get roomId from url
     const roomId = req.query.roomId;
     const rooms = readChatRoomsDB();
-
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
     //check if roomId exist
+    if (roomIdx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      const listMessages = rooms[roomIdx].messages;
 
-    //validate body
-    if (typeof req.body.text !== "string" || req.body.text.length === 0)
-      return res.status(400).json({ ok: false, message: "Invalid text input" });
+      //validate body
+      if (typeof req.body.text !== "string" || req.body.text.length === 0)
+        return res
+          .status(400)
+          .json({ ok: false, message: "Invalid text input" });
 
-    //create message
+      //create message
+      const text = req.body.text;
+      const newId = uuidv4();
+      const newMessage = {
+        messageId: newId,
+        text: text,
+        username: user.username,
+      };
+      listMessages.push(newMessage);
+      rooms[roomIdx].messages = listMessages;
 
-    writeChatRoomsDB(rooms);
+      writeChatRoomsDB(rooms);
+      return res.json({ ok: true, messages: newMessage });
+    }
   }
 }
